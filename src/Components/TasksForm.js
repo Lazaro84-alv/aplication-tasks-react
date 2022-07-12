03279e69b307848425ed21b7116490b15e38e3ae
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-07-12 11:41:16 -0300

    Implements in TasksForm components

--- src/Components/TasksForm.js
+++ src/Components/TasksForm.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/TasksForm.css";
+import { v4 as uuidv4 } from 'uuid';
 
 function TasksForm(props) {
+
+    const [input, setInput] = useState('');
+
+    const manageChange = e => {
+      setInput(e.target.value);
+      
+    };
+
+    const manageSend = e => {
+      e.preventDefault();
+
+      const newTask = {
+        id:  uuidv4(),
+        text: input,
+        completed: false
+      }
+
+      props.onSubmit(newTask);
+    };
+
     return (
-        <form className="tasks-form">
+        <form 
+          className="tasks-form"
+          onSubmit={manageSend}
+        >
             <input 
               className="tasks-input"
               type="text"
               placeholder="Write a task"
               name="text"
+              onChange={manageChange}
             />
             <button 
               className="tasks-button"
             >
-              Add Tasks
+              Add Task
             </button>
         </form>
     );
