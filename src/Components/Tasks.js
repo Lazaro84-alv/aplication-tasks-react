39ab434146214ba56a4c9b4b2ffb8376d0a9e81a
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-07-12 10:50:20 -0300

    Implements in Tasks components

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -2,13 +2,19 @@ import React from "react";
 import "../styles/Tasks.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Tasks({ id, text, completed, completeTasks, tasksDelete }) {
+function Tasks({ id, text, completed, completeTask, taskDelete }) {
     return (
         <div className={completed ? "tasks-container completed" : "tasks-container"}>
-            <div className="tasks-text">
+            <div 
+              className="tasks-text"
+              onClick={() => completeTask(id)}
+            >
                 {text}
             </div>
-            <div className="tasks-icon-container">
+            <div 
+              className="tasks-icon-container"
+              onClick={() => taskDelete(id)}
+              >
                 <AiOutlineCloseCircle className="tasks-icon" />
             </div>
         </div>
--- src/Components/TasksList.js
+++ src/Components/TasksList.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import TasksForm from "./TasksForm";
 import "../styles/TasksList.css";
+import Tasks from "./Tasks";
 
 function TasksList() {
+
+    const [tasks, setTasks] = useState([]);
+
     return (
         <>
             <TasksForm />
             <div className="tasks-list-container">
-                Tasks List
+              {
+                tasks.map((tasks) => 
+                  <Tasks 
+                    text={tasks.text}
+                    completed={tasks.completed}
+                  />
+                )
+              }
             </div>
         </>
     );
